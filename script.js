76b0efa71e4048150ceb1bdfe67e08c4f1b6670f
Guiziin0909 <132912110+Guiziin0909@users.noreply.github.com>
2024-11-16 13:09:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+let vida1 = 166;
+let vida2 = 200;
+vidaCompletaOponente = (vida1*100)/90
+function showIncon(image){
+    const icon = image.nextElementSibling;
+    icon.style.display = 'block';
+}
+
+function hideIcon(image){
+    const icon = image.nextElementSibling;
+    icon.style.display = 'none';
+}
+
+function setaa(){
+    document.getElementById('setaLuta').style.display = 'block';
+}
+function setae(){
+    document.getElementById('setaLuta').style.display = 'none';
+}
+
+function ataque(){
+    document.getElementById('ataqueJogador').style.display = 'block';
+}
+
+function esconderAtaque(){
+    document.getElementById('ataqueJogador')
+    .style.display = 'none';
+}
+
+    function ataque1(){
+        esconderAtaque();
+    
+        vida1 = vidaCompletaOponente
+        novaVidaOponente = vida1 - 20;
+        vida1 = novaVidaOponente
+        novaVidaP = (100*novaVidaOponente)/vidaCompletaOponente;
+        console.log(vida1)
+
+        document.getElementById('vidapkmOponente').style.width=`${novaVidaP}%`;
+    }
+    function ataque2(){
+        esconderAtaque();
+        vida1 = vidaCompletaOponente
+        novaVidaOponente = vida1 - 20;
+        vida1 = novaVidaOponente
+        novaVidaP = (100*novaVidaOponente)/vidaCompletaOponente;
+        console.log(vida1)
+
+        document.getElementById('vidapkmOponente').style.width=`${novaVidaP}%`;
+    }
+    function ataque3(){
+        esconderAtaque();
+        vida1 = vidaCompletaOponente
+        novaVidaOponente = vida1 - 20;
+        vida1 = novaVidaOponente
+        novaVidaP = (100*novaVidaOponente)/vidaCompletaOponente;
+        console.log(vida1)
+
+        document.getElementById('vidapkmOponente').style.width=`${novaVidaP}%`;
+    }
+    function ataque4(){
+        esconderAtaque();
+        vida1 = vidaCompletaOponente
+        novaVidaOponente = vida1 - 20;
+        vida1 = novaVidaOponente
+        novaVidaP = (100*novaVidaOponente)/vidaCompletaOponente;
+        console.log(vida1)
+
+        document.getElementById('vidapkmOponente').style.width=`${novaVidaP}%`;
+    }
+
